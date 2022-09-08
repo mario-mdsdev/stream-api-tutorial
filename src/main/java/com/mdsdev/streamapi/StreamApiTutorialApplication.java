f6582af852d57fffb1c4e6b37744148aca7a2c0a
Mário dos Santos <mario@mdsdev.com.br>
2022-09-07 21:34:16 -0300

    First commit

---
+++ src/main/java/com/mdsdev/streamapi/StreamApiTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.mdsdev.streamapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamApiTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamApiTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mdsdev/streamapi/StreamApiTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdsdev.streamapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamApiTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
